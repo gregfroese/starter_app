40914278fb2c07297af3f37ba4ec873d0b5f3b18
Greg Froese & Joanna Zeta <team+gregfroese+jzeta@roguerails.com>
2013-10-24 13:01:14 -0700

    update schema w/ projects table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131024164019) do
+ActiveRecord::Schema.define(version: 20131024172203) do
+
+  create_table "projects", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "statuses", force: true do |t|
     t.string   "name"
