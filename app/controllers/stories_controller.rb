f88334190781b73ed160402fb32ce243be000167
Adam Cuppy & Rob Burns <team+adam+rob@roguerails.com>
2013-10-24 13:47:10 -0700

    cleanup scaffolded code

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -1,26 +1,14 @@
 class StoriesController < ApplicationController
   before_action :set_story, only: [:show, :edit, :update, :destroy]
 
-  # GET /stories
-  def index
-    @stories = Story.all
-  end
-
-  # GET /stories/1
-  def show
-  end
-
-  # GET /stories/new
   def new
     @story = Story.new
   end
 
-  # GET /stories/1/edit
   def edit
     puts "HERE I AM"
   end
 
-  # POST /stories
   def create
     @story = Story.new(story_params)
 
@@ -31,7 +19,6 @@ class StoriesController < ApplicationController
     end
   end
 
-  # PATCH/PUT /stories/1
   def update
     if @story.update(story_params)
       puts story_params
@@ -41,19 +28,11 @@ class StoriesController < ApplicationController
     end
   end
 
-  # DELETE /stories/1
-  def destroy
-    @story.destroy
-    redirect_to stories_url, notice: 'Story was successfully destroyed.'
-  end
-
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_story
       @story = Story.find(params[:id])
     end
 
-    # Only allow a trusted parameter "white list" through.
     def story_params
       params.require(:story).permit(:goal, :stakeholder, :behavior, :business_value, :complexity_value, :status_id)
     end
