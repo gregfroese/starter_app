e15cadebcf5192c4eabb7b309b5b3c865fc07e61
Greg Froese <greg.froese@westernlife.com>
2013-11-04 10:38:47 -0600

    removed unused files

--- spec/controllers/criteria_comments_controller_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'spec_helper'
-
-describe CriteriaCommentsController do
-
-end
--- spec/models/criteria_comment_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'spec_helper'
-
-describe CriteriaComment do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
